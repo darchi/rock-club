f988c2da1a3695cfd206b1fda7046c4a4891b1ab
Konstantin <kukharev.k@toledo24.ru>
2020-05-12 22:45:35 +0300

    Updates

---
+++ precache-manifest.fffc835b34da25f03d5d142a78db56aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfa9796e1715878346908d2928da36c1",
+    "url": "/rock-club/index.html"
+  },
+  {
+    "revision": "6248999757a1aa84ed84",
+    "url": "/rock-club/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "17c79084c1e20441ba34",
+    "url": "/rock-club/static/js/2.a489c2db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rock-club/static/js/2.a489c2db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6248999757a1aa84ed84",
+    "url": "/rock-club/static/js/main.d3c8f854.chunk.js"
+  },
+  {
+    "revision": "dd97d4ee1627fda5bd14",
+    "url": "/rock-club/static/js/runtime-main.1c1344d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rock-club/static/media/logo.5d5d9eef.svg"
+  }
+]);
